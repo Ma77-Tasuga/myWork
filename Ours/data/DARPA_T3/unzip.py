97ae74726cf1e85ce1d1e495c66d2e47cee4bee0
xu_rui <matt.tasuga@gmail.com>
2024-11-06 16:44:37 +0800

    add data parsing

---
+++ Ours/data/DARPA_T3/unzip.py
@@ -0,0 +1,17 @@
+"""
+    step 1
+    解压文件
+"""
+
+
+import os
+
+# TODO: 注意修改文件路径
+# os.system('tar -zxvf ./zipdata/ta1-cadets-e3-official.json.tar.gz -C ./unziped_data')
+# os.system('tar -zxvf ./zipdata/ta1-cadets-e3-official-2.json.tar.gz -C ./unziped_data')
+# os.system('tar -zxvf ./zipdata/ta1-fivedirections-e3-official-2.json.tar.gz -C ./unziped_data')
+# os.system('tar -zxvf ./zipdata/ta1-theia-e3-official-1r.json.tar.gz -C ./unziped_data')
+# os.system('tar -zxvf ./zipdata/ta1-theia-e3-official-6r.json.tar.gz -C ./unziped_data')
+# os.system('tar -zxvf ./zipdata/ta1-trace-e3-official-1.json.tar.gz -C ./unziped_data')
+
+os.system('tar -zxvf ./zip_data/ta1-trace-e3-official-1.json.tar.gz -C ./raw_data')
